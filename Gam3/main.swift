b2446a2ff6ca912a4ebfac242307f84d2e962fd2
Arthur Berschader <iuniqu3@icloud.com>
2016-08-31 03:03:47 +0200

    Initial Commit

---
+++ Gam3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Gam3
+//
+//  Created by Arthur Berschader on 31.08.16.
+//  Copyright © 2016 Arthur / Theo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
